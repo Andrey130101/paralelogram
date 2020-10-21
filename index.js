74b4a3ca0e7e8c8483ae620fadf3ade378c9d1f4
Andrey130101 <andreyrogovchenko2@gmail.com>
2020-10-21 12:55:55 +0300

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/static'));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
-
+//process.env.PORT
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port ");
 });
